e2674f2948c192cb86b2ef25e10b46a6d0c62c92
Lu Li <lilu83@mail.sysu.edu.cn>
2026-04-17 11:20:34 +0800

    type error

--- main/DA/MOD_DA_Const.F90
+++ main/DA/MOD_DA_Const.F90
@@ -29,9 +29,9 @@ MODULE MOD_DA_Const
    ! Constant variables
    real(r8), parameter :: C = 2.998e8           ! speed of light (m/s)
    real(r8), parameter :: mu0 = 4.*pi*1e-7      ! vacuum permeability (H/m)
-   real(r8), parameter :: z0 = sqrt(mu0/eps0)   ! impedance of free space (Ohm)
-   real(r8), parameter :: eps_w_inf = 4.9       ! dielectric constant at infinite frequency (Stogryn 1971),
    real(r8), parameter :: eps_0 = 8.854e-12     ! dielectric constant of free space (Klein and Swift 1977) [Farads/meter]
+   real(r8), parameter :: z0 = sqrt(mu0/eps_0)  ! impedance of free space (Ohm)
+   real(r8), parameter :: eps_w_inf = 4.9       ! dielectric constant at infinite frequency (Stogryn 1971),
    real(r8), parameter :: rho_soil = 2.66       ! soil specific density (g/cm3)
    real(r8), parameter :: f0w = 9.              ! relaxation frequency of liquid water (GHz)
    real(r8), parameter :: rgh_surf = 2.2        ! soil surface roughness (cm)
